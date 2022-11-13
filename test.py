2951eb8d360d41cca298e4ea57c0855a0f74c264
suhaas <footballandgermany@gmail.com>
2022-11-13 11:18:13 -0500

    final stages

---
+++ test.py
@@ -0,0 +1,9 @@
+from utils import *
+
+def test_getDataFromAPI(self):
+    api = "https://dummyjson.com/"
+    products_api = api+"products"
+    users_api = api+"users"
+    order_api = api+"carts"
+    products
+    getDataFromAPI()
